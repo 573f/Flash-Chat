e0c083e330650750de003629a49d391fdbddf1e7
Stephen Skubik-Peplaski <codesmith@me.com>
2017-09-02 14:37:56 -0400

    Completed user registration with seque and secure input

--- Flash Chat/RegisterViewController.swift
+++ Flash Chat/RegisterViewController.swift
@@ -6,16 +6,12 @@
 //
 
 import UIKit
-
+import Firebase
 
 class RegisterViewController: UIViewController {
 
-    
-    //Pre-linked IBOutlets
-
     @IBOutlet var emailTextfield: UITextField!
     @IBOutlet var passwordTextfield: UITextField!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,19 +21,14 @@ class RegisterViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
-
-  
     @IBAction func registerPressed(_ sender: AnyObject) {
-        
-
-        
-        //TODO: Set up a new user on our Firbase database
-        
-        
-
-        
-        
+        Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in
+            if error != nil {
+                print(error as Any)
+            } else {
+                self.performSegue(withIdentifier: "goToChat", sender: self)
+            }
+        }
     } 
-    
     
 }
